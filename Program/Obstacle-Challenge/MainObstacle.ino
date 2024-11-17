82d8f172735ab15555e62a4ac822f08d00ddd3c5
Book2009 <142088056+Book2009@users.noreply.github.com>
2024-11-17 14:09:57 +0700

    Update MainObstacle.ino

--- Program/Obstacle-Challenge/MainObstacle.ino
+++ Program/Obstacle-Challenge/MainObstacle.ino
@@ -1,144 +1,242 @@
-#include <Servo.h>
-#include "Mapf.h"
+#include <Mapf.h>
 #include <PID_v2.h>
+#include <Wire.h>
+#include <Servo.h>
+#include "CameraHandler.h"
+
+// SETUP OPENMV CAMERA
+CameraHandler camera;
+BlobData blob;
+BlobData purple_blob1;
+BlobData purple_blob2;
+
+// SETUP SERVO
 Servo myservo;
 Servo myservo2;
+int const STEER_SRV = 27;  //16
+int const ULTRA_SRV = 25;  //23
 
-//Motor
+// SETUP MOTORS
 const int E1Pin = 10;
 const int M1Pin = 12;
+const int E2Pin = 11;
+const int M2Pin = 13;
+
 typedef struct {
   byte enPin;
   byte directionPin;
 } MotorContrl;
+
 const int M1 = 0;
 const int M2 = 1;
-const int MotorNum = 1;
+const int MotorNum = 2;
 
-const MotorContrl MotorPin[] = { { E1Pin, M1Pin } };
+const MotorContrl MotorPin[] = { { E1Pin, M1Pin }, { E2Pin, M2Pin } };
 
 const int Forward = LOW;
 const int Backward = HIGH;
 
 //Button
 int BUTTON = A8;
-// ultra_servo(pvYaw, 'R');
 
 //PID
-PID_v2 compassPID(0.65, 0.0001, 0.035, PID::Direct);
+PID_v2 compassPID(0.69, 0, 0, PID::Direct);
 
 //Ultra
 int const ULTRA_PIN = A9;
+int const ULTRA_PIN_II = A10;
+
 
 //INEX Gyro
-float pvYaw, pvRoll, pvPitch;
+float pvYaw;
 uint8_t rxCnt = 0, rxBuf[8];
 
 //  Light Sensors
 int const RED_SEN = A6;
 int const BLUE_SEN = A7;
 
-// Servo
-int const STEER_SRV = 16;
-int const ULTRA_SRV = 23;
-
-//OpenMV
-static String receivedString = "";  // Buffer to store incoming data
-int tempXBlob = -1, tempYBlob = -1, tempWidthBlob = -1, tempHeightBlob = -1;
-int tempXRedBlob = -1, tempYRedBlob = -1, tempWidthRedBlob = -1, tempHeightRedBlob = -1;
-
-int largestBlobX = 0, largestBlobY = 0, largestBlobWidth = 0, largestBlobHeight = 0;
-int largestBlobArea = 0;
-
-bool found_block;
-float avoidance_degree = 0;
-int missedBlockCount = 0;
-const int MAX_MISSED_BLOCKS = 5;
-int falseDetectionCount = 0;
-const int requiredFalseCount = 3;
-
 //Others
-char TURN = 'U';
-char last_block = 'U';
-char Blocks_TURN = 'U';
-char before_last_block = 'U';
-long halt_detect_line_timer;
-int Line_Number = 0;
+char TURN = 'U';  // L = left, R = Right, U = undefined
+char ULTRA_DIR = 'R';
+int last_found_signature;
 int plus_degree = 0;
-long MV_timer = 0;
-int block_count = 0;
-float blockCenterX;
-long timer_block_decay;
-float found_block_factor;
-int signature;
-int blockDetectionCounter = 0;
-const int detectionThreshold = 5;  // Number of consecutive detections needed
-float pidOutput;
-float baseDesiredDistance = 28.5;
+double halt_detect_line_timer = 0;
+double halt_detect_mok = 0;
+double MV_timer = 0;
+int count_line = 0;
+int parking_step = 0;
+int parkingsection = -1;
+bool fame = false;
+bool parking = false;
+bool next = false;
+bool foundpark = false;
+float found_parkAngle = 0;
+char lastblock;
+char MOK = 'U';
+int side = 1;
+float Kaopun;
+float Sin;
+float Gain;
+bool neab = false;
+float avoidance_degree;
+int hi = 0;
+char currentBlock = 'N';
+char previousBlock = 'N';
+int angle = 80;
+int ignore_line = -1;
+int INT;
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(19200);
-  Serial3.begin(19200);
-
-  compassPID.Start(0, 0, 0);
-  compassPID.SetOutputLimits(-180, 180);
-  compassPID.SetSampleTime(10);
-
-  pinMode(STEER_SRV, OUTPUT);
-  pinMode(ULTRA_SRV, OUTPUT);
-  pinMode(ULTRA_PIN, INPUT);
-  pinMode(RED_SEN, INPUT);
-  pinMode(BLUE_SEN, INPUT);
-  pinMode(BUTTON, INPUT);
-
-  initMotor();
-  while (!Serial)
-    ;
-  myservo.attach(ULTRA_SRV, 500, 2400);
-  myservo2.attach(STEER_SRV, 500, 2500);
-  steering_servo(0);
-  ultra_servo(0, 'L');
+  initialize_everything();
   while (analogRead(BUTTON) > 500)
     ;
   zeroYaw();
 }
 
 void loop() {
-  while (analogRead(BUTTON) > 500) {
-    getIMU();
-    Color_detection();
-    ultra_servo(pvYaw, TURN);
-    float desiredDistance = baseDesiredDistance;
-    if (TURN == 'L') {
-      desiredDistance = 37.5;
-    } else if (TURN == 'R') {
-      desiredDistance = 37.5;
-    }
-    float distanceError = getDistance() - desiredDistance;
-    float deadband = 2.0;
-    if (abs(distanceError) < deadband) {
-      distanceError = 0.0;
-    }
+  // Calculate camera errors
+  camera.handleIncomingData();
+  BlobData tempBlob = camera.getBlobData();
+  int desiredDistance = parking_step == 0 ? (camera.isBlockFound() ? 20 : 40) : 15;
+  if (tempBlob.signature == 1) {
+    // RED
+    last_found_signature = 1;
+    blob = tempBlob;
 
-    float directionFactor = (TURN == 'L') ? -1.0 : 1.0;
-    float adjustedYaw = pvYaw + (distanceError * directionFactor);
-    float pidOutput = compassPID.Run(adjustedYaw) * -1.1;
+  } else if (tempBlob.signature == 2) {
+    // GREEN
+    last_found_signature = 2;
+    blob = tempBlob;
 
-    if (millis() - MV_timer > 50) {
-      handleIncomingData();
-      avoidance_degree = calculate_avoidance();
-      // Serial.println(avoidance_degree);
-      MV_timer = millis();
-    }
-    if (avoidance_degree != 0) {
-      int final_degree = mapf(min(max(getDistance(), 5), 28), 5, 28, pidOutput, avoidance_degree);
-      steering_servo(final_degree);
-    } else {
-      steering_servo(pidOutput);
-    }
-    motor(70);
-    // Serial.println(final)
-    // motor_and_steer(final_degree);
+  } else if (tempBlob.signature == 3) {
+    purple_blob1 = tempBlob;
+  } else if (tempBlob.signature == 4) {
+    purple_blob2 = tempBlob;
   }
+
+  float distanceError = getDistance() - desiredDistance;
+  float frontDistance = getDistanceII();
+  float avoidance_degree = 0;
+  if (tempBlob.signature == 3 && tempBlob.width / 3.9 > blob.width) {
+    avoidance_degree = calculate_avoidance(tempBlob.signature, tempBlob.width, tempBlob.x, tempBlob.y);
+  } else {
+    avoidance_degree = calculate_avoidance(blob.signature, blob.width, blob.x, blob.y);
+  }
+
+
+  // Calculate distance errors
+  float deadband = 2.0;
+  if (abs(distanceError) < deadband) {
+    distanceError = 0.0;
+  }
+  if (parking_step == 1) {
+    ULTRA_DIR = TURN == 'R' ? 'L' : 'R';
+  }
+  float directionFactor = (ULTRA_DIR == 'R') ? -1.0 : 1.0;
+  float adjustedYaw = pvYaw - clamp(distanceError * directionFactor, -20, 20);
+  float pidOutput = compassPID.Run(adjustedYaw);
+
+  // Calculate Compass errors
+  getIMU();
+  color_detection();
+
+  // TEST PARKING
+
+  if (count_line >= 8 && count_line < 12 && tempBlob.signature == 3 && parkingsection == -1) {
+    parkingsection = count_line % 4;
+  } else if (count_line > 12) {
+    if (parkingsection == 0) {
+      parkingsection = 4;
+    }
+  }
+  int parking_degree = ((purple_blob1.x + purple_blob2.x) / 2 - 160) * -0.5;
+  int final_degree = camera.isBlockFound() ? mapf(min(max(getDistance(), 15), desiredDistance), 15, desiredDistance, pidOutput, avoidance_degree) : pidOutput;
+
+  switch (parking_step) {
+    case 1:
+
+      if (!fame) {
+        halt_detect_mok = millis();
+        fame = true;
+      }
+      if (tempBlob.signature != 3 || tempBlob.width < 70) {
+        steering_servo(pidOutput * 1.3);
+        ultra_servo(pvYaw, ULTRA_DIR);
+        motor(40);
+      } else {
+        parking_step = 2;
+        fame = false;
+      }
+      break;
+    case 2:
+      halt_detect_line_timer = millis();
+      if (!fame) {
+        halt_detect_mok = millis();
+        fame = true;
+      }
+      if (millis() - halt_detect_mok < 1400) {
+        steering_servo(final_degree);
+        ultra_servo(0, ULTRA_DIR);
+        motor(40);
+        // Serial.println("1");
+      } else {
+        parking_step = 3;
+        fame = false;
+        if (TURN == 'L') {
+          plus_degree += 90;
+        } else {
+          plus_degree -= 90;
+        }
+      }
+
+      break;
+
+    case 3:
+
+      if (!fame) {
+        halt_detect_mok = millis();
+        fame = true;
+      }
+      if (millis() - halt_detect_mok < 2000) {
+        steering_servo(pvYaw);
+        ultra_servo(0, ULTRA_DIR);
+        motor(-40);
+      } else {
+        parking_step = 4;
+        fame = false;
+      }
+
+      break;
+
+    case 4:
+      if (frontDistance > 10) {
+        steering_servo(mapf(clamp(frontDistance, 10, 20), 20, 10, parking_degree, 0));
+        motor(mapf(clamp(frontDistance, 10, 20), 20, 10, 50, 40));
+      } else {
+        motor(30);
+        delay(500);
+        motor(0);
+        while (true)
+          ;
+      }
+      break;
+
+    case 5:
+      if (count_line >= 13 && count_line < 12 + parkingsection) {
+        motor_and_steer(pidOutput);
+        ultra_servo(pvYaw, TURN);
+      } else {
+        parking_step = 1;
+      }
+      break;
+
+    default:
+      motor_and_steer(final_degree);
+      ultra_servo(pvYaw, ULTRA_DIR);
+      if (count_line > 12 && parkingsection != -1) {
+        parking_step = 5;
+      }
+      break;
+  }
+  uTurn();
 }
