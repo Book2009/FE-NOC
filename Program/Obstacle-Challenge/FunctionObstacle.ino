30b93786baa2a9fe2577a681ebfc4f8f11191f29
Book2009 <142088056+Book2009@users.noreply.github.com>
2024-11-17 15:46:46 +0700

    Update FunctionObstacle.ino

--- Program/Obstacle-Challenge/FunctionObstacle.ino
+++ Program/Obstacle-Challenge/FunctionObstacle.ino
@@ -1,66 +1,27 @@
-void handleIncomingData() {
+void initialize_everything() {
+  Serial.begin(19200);
+  Serial1.begin(19200);
+  Serial2.begin(19200);
+  Serial3.begin(19200);
 
-  found_block = false;  // Reset found_block at the start
-  signature = 0;
-  while (Serial3.available()) {
-    char incomingByte = Serial3.read();
-    if (incomingByte != '\n') {
-      receivedString += incomingByte;
-    } else {
-      receivedString.trim();  // Remove any leading or trailing whitespace
+  compassPID.Start(0, 0, 0);
+  compassPID.SetOutputLimits(-180, 180);
+  compassPID.SetSampleTime(10);
 
-      // Parse the received data
-      int commaIndex1 = receivedString.indexOf(',');
-      int commaIndex2 = receivedString.indexOf(',', commaIndex1 + 1);
-      int commaIndex3 = receivedString.indexOf(',', commaIndex2 + 1);
-      int commaIndex4 = receivedString.indexOf(',', commaIndex3 + 1);
+  pinMode(STEER_SRV, OUTPUT);
+  pinMode(ULTRA_SRV, OUTPUT);
+  pinMode(ULTRA_PIN, INPUT);
+  pinMode(RED_SEN, INPUT);
+  pinMode(BLUE_SEN, INPUT);
+  pinMode(BUTTON, INPUT);
 
-      if (commaIndex1 != -1 && commaIndex2 != -1 && commaIndex3 != -1 && commaIndex4 != -1) {
-        int xValue = receivedString.substring(0, commaIndex1).toInt();
-        int yValue = receivedString.substring(commaIndex1 + 1, commaIndex2).toInt();
-        int widthValue = receivedString.substring(commaIndex2 + 1, commaIndex3).toInt();
-        int heightValue = receivedString.substring(commaIndex3 + 1, commaIndex4).toInt();
-        int blobType = receivedString.substring(commaIndex4 + 1).toInt();
-
-        // Validate the coordinates and dimensions
-        if (xValue >= 0 && xValue <= 320 && yValue >= 0 && yValue <= 240 && widthValue >= 0 && widthValue <= 320 && heightValue >= 0 && heightValue <= 240) {
-          // Store the data of the received blob
-          largestBlobX = xValue;
-          largestBlobY = yValue;
-          largestBlobWidth = widthValue;
-          largestBlobHeight = heightValue;
-          signature = (blobType == 1) ? 2 : 1;  // 2 for red, 1 for general
-
-          found_block = true;  // A valid blob was received
-          // Serial.println("Valid blob detected and stored.");
-        } else {
-          Serial.println("Received invalid coordinates or dimensions.");
-          found_block = false;
-        }
-      } else {
-        Serial.println("Data format error.");
-        found_block = false;
-      }
-      receivedString = "";  // Clear the received string after processing
-    }
-  }
-
-  if (found_block) {
-    // Serial.print("Blob - X: ");
-    // Serial.print(largestBlobX);
-    // Serial.print(", Y: ");
-    // Serial.print(largestBlobY);
-    // Serial.print(", Width: ");
-    // Serial.print(largestBlobWidth);
-    // Serial.print(", Height: ");
-    // Serial.print(largestBlobHeight);
-    // Serial.print(", FoundBlock: ");
-    // Serial.print(found_block);
-    // Serial.print(", Signature: ");
-    // Serial.println(signature);
-  } else {
-    Serial.println("No valid blob detected.");
-  }
+  initMotor();
+  while (!Serial)
+    ;
+  myservo.attach(ULTRA_SRV, 500, 2400);
+  myservo2.attach(STEER_SRV, 500, 2500);
+  steering_servo(0);
+  ultra_servo(0, 'U');
 }
 
 float degreesToRadians(double degrees) {
@@ -71,73 +32,51 @@ float radiansToDegree(double raidans) {
   return raidans / PI * 180.0;
 }
 
-float calculate_avoidance() {
-  // Check if a block is found
-  if (signature != 0) {
-    // Reset missed block count if a block is found
-    missedBlockCount = 0;
+float _cal_avoidance(char mode, int targetWidth, int objectWidth, int blockCenterX, int blockCenterY) {
+  float focalLength = 2.8;
+  float cameraFOV = 70;
 
-    // Constants for OpenMV H7
-    int targetHeight = 10;    // Height of the object in centimeters
-    float focalLength = 3.6;  // Focal length of the OpenMV H7 camera lens in mm
-    float cameraFOV = 60.0;   // Field of view of the OpenMV H7 camera in degrees
+  float distance = (targetWidth * focalLength * 100) / objectWidth;
 
-    // Use the largest blob data
-    int objectHeight = largestBlobHeight;
-    float distance = (targetHeight * focalLength * 100) / objectHeight;
+  float deltaX = blockCenterX - (320 / 2);
+  float deltaY = blockCenterY - (240 / 2);
 
-    float blockCenterX = largestBlobX;
-    float blockCenterY = largestBlobY;
+  float detected_degree = -deltaX * cameraFOV / 320.0;
 
-    float deltaX = blockCenterX - (320 / 2);  // Assuming a 640x480 image resolution
-    float deltaY = blockCenterY - (240 / 2);
+  float blockPositionX = distance * sin(degreesToRadians(detected_degree));
+  float blockPositionY = distance * cos(degreesToRadians(detected_degree)) - 10;
 
-    float detected_degree = -deltaX * cameraFOV / 320.0;  // Inverted deltaX to swap behavior
+  ULTRA_DIR = mode;
 
-    float blockPositionX = distance * sin(degreesToRadians(detected_degree));
-    float blockPositionY = distance * cos(degreesToRadians(detected_degree)) - 18;
-
-    // Adjust avoidance degrees based on blob signature
-    if (signature == 1) {
-      avoidance_degree = max(radiansToDegree(atan2(blockPositionX + 9, blockPositionY)), 5) * -1.4;
-      last_block = 'L';
-      Blocks_TURN == 'L';
-      before_last_block = 'L';
-      //avoidance_degree = 0;
-    } else if (signature == 2) {
-      avoidance_degree = min(radiansToDegree(atan2(blockPositionX - 9, blockPositionY)), -5) * -1.7;
-      last_block = 'R';
-      Blocks_TURN == 'R';
-      before_last_block = 'R';
-      //avoidance_degree = 0;
-    }
+  if (mode == 'L') {
+    return max(radiansToDegree(atan2(blockPositionX + (targetWidth / 2 + 10), blockPositionY)), 5) * 0.85;
+  } else if (mode == 'R') {
+    return min(radiansToDegree(atan2(blockPositionX - (targetWidth / 2 + 10), blockPositionY)), -5) * 1;
   } else {
-    // Increment the missed block count if no block is found
-    missedBlockCount++;
+    return 0;
+  }
+}
 
-    // // If the block is missed three times in a row, set avoidance degree to zero
-    if (missedBlockCount >= MAX_MISSED_BLOCKS) {
-      avoidance_degree = 0;  // or some default value
-    }
+float calculate_avoidance(int signature, int objectWidth, int blockCenterX, int blockCenterY) {
+  int avoidance_degree = 0;
+
+  if (signature == 2) {
+    avoidance_degree = _cal_avoidance('L', 5, objectWidth, blockCenterX, blockCenterY);
+  } else if (signature == 1) {
+    avoidance_degree = _cal_avoidance('R', 5, objectWidth, blockCenterX, blockCenterY);
+  } else if (signature == 3 || signature == 4) {
+    avoidance_degree = _cal_avoidance(TURN, 20, objectWidth, blockCenterX, blockCenterY);
+    Serial.println(avoidance_degree);
   }
   return avoidance_degree;
 }
+
 int wrapValue(int value, int minValue, int maxValue) {
   int range = maxValue - minValue + 1;
   if (value < minValue) {
     value += range * ((minValue - value) / range + 1);
   }
   return minValue + (value - minValue) % range;
-}
-
-void motor(int speed) {
-  if (speed > 0) {
-    setMotorDirection(M1, Forward);
-    setMotorSpeed(M1, speed);
-  } else {
-    setMotorDirection(M1, Backward);
-    setMotorSpeed(M1, speed);
-  }
 }
 
 void initMotor() {
@@ -150,62 +89,29 @@ void initMotor() {
   }
 }
 
-/**  motorNumber: M1, M2
-direction:          Forward, Backward **/
+void motor(int speed) {
+  if (speed > 0) {
+    setMotorDirection(M1, Forward);
+    setMotorSpeed(M1, speed);
+  } else {
+    setMotorDirection(M1, Backward);
+    setMotorSpeed(M1, speed);
+  }
+}
+
 void setMotorDirection(int motorNumber, int direction) {
   digitalWrite(MotorPin[motorNumber].directionPin, direction);
 }
 
-/** speed:  0-100   * */
 inline void setMotorSpeed(int motorNumber, int speed) {
-  analogWrite(MotorPin[motorNumber].enPin, 255.0 * (speed / 100.0));  //PWM
+  analogWrite(MotorPin[motorNumber].enPin, 255.0 * (speed / 100.0));
 }
 
-void zeroYaw() {
-  Serial1.begin(115200);
-  delay(100);
-  // Sets data rate to 115200 bps
-  Serial1.write(0XA5);
-  delay(10);
-  Serial1.write(0X54);
-  delay(100);
-  // pitch correction roll angle
-  Serial1.write(0XA5);
-  delay(10);
-  Serial1.write(0X55);
-  delay(100);
-  // zero degree heading
-  Serial1.write(0XA5);
-  delay(10);
-  Serial1.write(0X52);
-  delay(100);
-  // automatic mode
-}
-
-bool getIMU() {
-  while (Serial1.available()) {
-    rxBuf[rxCnt] = Serial1.read();
-    if (rxCnt == 0 && rxBuf[0] != 0xAA) return;
-    rxCnt++;
-    if (rxCnt == 8) {  // package is complete
-      rxCnt = 0;
-      if (rxBuf[0] == 0xAA && rxBuf[7] == 0x55) {  // data package is correct
-        pvYaw = (int16_t)(rxBuf[1] << 8 | rxBuf[2]) / 100.f;
-        pvPitch = (int16_t)(rxBuf[3] << 8 | rxBuf[4]) / 100.f;
-        pvRoll = (int16_t)(rxBuf[5] << 8 | rxBuf[6]) / 100.f;
-        pvYaw = wrapValue(pvYaw + plus_degree, -179, 180);
-        return true;
-      }
-    }
-  }
-  return false;
-}
-
-void Color_detection() {
+void color_detection() {
   int blue_value = analogRead(BLUE_SEN);
   if (TURN == 'U') {
     int red_value = analogRead(RED_SEN);
-    if (blue_value < 605 || red_value < 400) {
+    if (blue_value < 600 || red_value < 600) {
       int lowest_red_sen = red_value;
       long timer_line = millis();
       while (millis() - timer_line < 100) {
@@ -214,53 +120,45 @@ void Color_detection() {
           lowest_red_sen = red_value;
         }
       }
-      if (lowest_red_sen > 400) {
+      if (lowest_red_sen > 600) {
         // Red
-        TURN = 'L';
+        TURN = 'R';
         plus_degree += 90;
       } else {
         // Blue
-        TURN = 'R';
+        TURN = 'L';
         plus_degree -= 90;
       }
       halt_detect_line_timer = millis();
-      Line_Number++;
+      count_line++;
     }
   } else {
-    if (millis() - halt_detect_line_timer > 1500) {
-      Serial.println(pvYaw);
-      if (blue_value < 605) {
-        if (TURN == 'R') {
+    if (millis() - halt_detect_line_timer > 1800) {
+      if (blue_value < 600) {
+        if (TURN == 'L') {
           plus_degree -= 90;
         } else {
           plus_degree += 90;
         }
         halt_detect_line_timer = millis();
-        Line_Number++;
+        count_line++;
       }
     }
   }
 }
 
 void steering_servo(int degree) {
-  myservo2.write((90 + max(min(degree, 45), -45)) / 2);
-}
-
-void motor_and_steer(int degree) {
-  degree = max(min(degree, 52), -52);
-  steering_servo(degree);
-  // motor((map(abs(degree), 0, 45, 60, 70)));
-  motor(0);
+  myservo2.write((90 + max(min(degree, 50), -50)) / 2);
 }
 
 void ultra_servo(int degree, char mode_steer) {
   int middle_degree = 0;
   if (mode_steer == 'F') {
-    middle_degree = 135;
-  } else if (mode_steer == 'L') {
-    middle_degree = 135 - 75;
-  } else if (mode_steer == 'R' || mode_steer == 'U') {
-    middle_degree = 135 + 95;
+    middle_degree = 150;
+  } else if (mode_steer == 'R') {
+    middle_degree = 225;
+  } else if (mode_steer == 'L' || mode_steer == 'U') {
+    middle_degree = 80;
   } else {
   }
   myservo.write(mapf(max(min(middle_degree + degree, 225), 45), 0, 270, 0, 180));
@@ -275,4 +173,144 @@ float getDistance() {
     raw_distance -= 0;
   }
   return min(raw_distance, 50);
+}
+
+float getDistanceII() {
+  float raw_distance = mapf(analogRead(ULTRA_PIN_II), 0, 1023, 0, 500);
+  if (TURN == 'L') {
+    raw_distance += 0;
+  } else if (TURN == 'R') {
+    raw_distance -= 0;
+  }
+  return min(raw_distance, 50);
+}
+
+bool getIMU() {
+  while (Serial1.available()) {
+    rxBuf[rxCnt] = Serial1.read();
+    if (rxCnt == 0 && rxBuf[0] != 0xAA) return;
+    rxCnt++;
+    if (rxCnt == 8) {
+      rxCnt = 0;
+      if (rxBuf[0] == 0xAA && rxBuf[7] == 0x55) {
+        pvYaw = (int16_t)(rxBuf[1] << 8 | rxBuf[2]) / 100.f;
+        pvYaw = wrapValue(pvYaw + plus_degree, -179, 180);
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+void zeroYaw() {
+  Serial1.begin(115200);
+  delay(100);
+  Serial1.write(0XA5);
+  delay(10);
+  Serial1.write(0X54);
+  delay(100);
+  Serial1.write(0XA5);
+  delay(10);
+  Serial1.write(0X55);
+  delay(100);
+  Serial1.write(0XA5);
+  delay(10);
+  Serial1.write(0X52);
+  delay(100);
+}
+
+void motor_and_steer(int degree) {
+  degree = clamp(degree, -52, 52);
+  steering_servo(degree);
+  motor((map(abs(degree), 0, 30, 49, 49)));
+}
+
+float clamp(float value, float minVal, float maxVal) {
+  if (value < minVal) return minVal;
+  if (value > maxVal) return maxVal;
+  return value;
+}
+
+
+void check_leds() {
+  while (true) {
+    Serial.print("Geen: ");
+    Serial.print(analogRead(BLUE_SEN));
+    Serial.print("   Red: ");
+    Serial.println(analogRead(RED_SEN));
+  }
+}
+
+void uTurn() {
+  camera.handleIncomingData();
+  BlobData tempBlob = camera.getBlobData();
+
+  if ((count_line == 3 && count_line < 4) || (count_line == 7 && count_line < 8)) {
+    if (tempBlob.signature == 1) {
+      currentBlock = 'R';
+    } else if (tempBlob.signature == 2) {
+      currentBlock = 'G';
+    } else {
+      currentBlock = 'N';
+    }
+    if (currentBlock != 'N') {
+      if (currentBlock != lastblock) {
+        previousBlock = lastblock;
+      } else {
+        previousBlock = currentBlock;
+      }
+    }
+  }
+  lastblock = currentBlock;
+  Serial.print(currentBlock);
+  Serial.print("lastblock");
+  Serial.println(lastblock);
+
+  if (count_line == 8) {
+    if (analogRead(BLUE_SEN) > 600) {
+      ignore_line = 1;
+    }
+    if (analogRead(BLUE_SEN) < 600 && ignore_line == 1) {
+      if (lastblock == 'R') {
+        count_line++;
+        if (TURN == 'R') {
+          TURN = 'L';
+          plus_degree += 90;
+        } else {
+          TURN = 'R';
+          plus_degree -= 90;
+        }
+        unsigned long timer = millis();
+        while (millis() - timer <= 8000) {
+          getIMU();
+          steering_servo(pvYaw);
+          motor(-50);
+        }
+        halt_detect_line_timer = millis();
+      }
+    } else {
+      if (tempBlob.signature == 1) {
+        currentBlock = 'R';
+      } else if (tempBlob.signature == 2) {
+        currentBlock = 'G';
+      } else {
+        currentBlock = 'N';
+      }
+      if (currentBlock != 'N') {
+        if (currentBlock != lastblock) {
+          previousBlock = lastblock;
+        } else {
+          previousBlock = currentBlock;
+        }
+      }
+    }
+  }
+}
+
+
+float angleDiff(float a, float b) {
+  float diff = a - b;
+  while (diff > 180) diff -= 360;
+  while (diff <= -180) diff += 360;
+  return diff;
 }
